4df44a09c5878f137d14cb149f616f15a186e6ae
saeedkmf2020 <golaaasi@gmail.com>
2024-08-23 19:08:31 +0330

    hi
    hi

---
+++ start-conda.ps1
@@ -0,0 +1,4 @@
+& 'C:\Users\yahol\anaconda3\shell\condabin\conda-hook.ps1'
+conda activate a11
+Set-Location "E:\Stable D\stable-diffusion-webui"
+webui-user.bat
--- webui-user.bat
+++ webui-user.bat
@@ -1,8 +1,9 @@
 @echo off
 
-set PYTHON=
+set PYTHON="C:\Users\yahol\AppData\Local\Programs\Python\Python312\python.exe"
 set GIT=
 set VENV_DIR=
+set COMMANDLINE_ARGS=--api --xformers --reinstall-xformers --api --xformers --reinstall-xformers 
+git pull
 set COMMANDLINE_ARGS=
-
 call webui.bat
